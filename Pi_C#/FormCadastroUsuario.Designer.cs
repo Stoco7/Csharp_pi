1576bec9a7ce24e9eb81421707a2b73796cda121
Eduardo Stoco <stoco.eduardo@gmail.com>
2025-03-19 16:12:18 -0300

    att

--- Pi_C#/FormCadastroUsuario.Designer.cs
+++ Pi_C#/FormCadastroUsuario.Designer.cs
@@ -210,7 +210,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(126, 19);
+            this.label10.Location = new System.Drawing.Point(118, 21);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(150, 25);
             this.label10.TabIndex = 19;
